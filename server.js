5f436106cb237540973d91b15f3f87f6dedb3970
pleaseWork <ellergabriel13@gmail.com>
2024-01-17 11:09:42 -0800

    added console logging for new requests)

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
 app.use('/', function(req,res){
+	console.log(`Request received at ${new Date()}`);
 	var fileName = req.url || "index.html";
 	var ext = path.extname(fileName)
-    res.sendFile(path.join(__dirname+'/express/' + fileName));
+    res.sendFile(path.join(__dirname+ '/express/' + fileName));
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
