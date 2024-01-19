9a2e045d5307a07c20f8bab703cda64beefbc2b9
pleaseWork <ellergabriel13@gmail.com>
2024-01-19 14:02:27 -0800

    added 404 error message when accessing unavailable content

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.json());
-app.use(express.static("views"));
+app.use(express.static(path.join(__dirname + "/public")));
 const server = http.createServer(app);
 const port = 3000;
 // default URL for website
-app.use('/', function(req,res){
+app.use(function(req,res){
 	//console.log(`Request received at ${new Date()}`);
 	//var fileName = req.url || "index.html";
 	//var ext = path.extname(fileName)
-   	res.sendFile(path.join(__dirname+ '/views/index.html'));
+	res.status(404);
+   	res.send(`<h1>Error 404: Resource not found</h1>`);
     //__dirname : It will resolve to your project folder.
   });
 app.get('/', function(req, res) {
