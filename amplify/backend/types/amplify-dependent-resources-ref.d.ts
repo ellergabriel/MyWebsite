5b7bf6ecb480a8c82c1d058cf3ee42575535ab8a
pleaseWork <ellergabriel13@gmail.com>
2024-02-08 10:28:10 -0800

    continuing amplify migration

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mywebsite": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
